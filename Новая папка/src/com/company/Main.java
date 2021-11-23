acdb377570c00ec71a039739f51a0ae01b6f56b2
greedman231 <93518502+greedman231@users.noreply.github.com>
2021-11-23 07:33:35 +0300

    T-0
    GrafualCastDemo

---
+++ Новая папка/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte x,z;
+        int y;
+
+        x = 5;
+        y=x * x;
+
+        z=(byte) (x * x);
+    }
+}
